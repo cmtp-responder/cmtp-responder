76d77b1fbda9a2cdfbdc7c71337c9705eeba6ae4
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-03-04 16:16:08 +0100

    all: Add Collabora copyright to the touched files
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- include/transport/mtp_descs_strings.h
+++ include/transport/mtp_descs_strings.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012, 2013, 2018 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2019 Collabora Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
