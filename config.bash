c7c28dd9a65aa94d0a9d5bde06a41191ee028046
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-03-27 08:02:16 +0100

    Enumeration tests
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

---
+++ config.bash
@@ -0,0 +1,8 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# (c) Collabora Ltd. 2019
+# Author: Andrzej Pietrasiewicz <andrzej.p@collabora.com>
+#
+
+DISCONNECT_CONNECT_COUNT=1
---
+++ mtp-device-id.bash
@@ -0,0 +1,18 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# (c) Collabora Ltd. 2019
+# Author: Andrzej Pietrasiewicz <andrzej.p@collabora.com>
+#
+
+MTP_DEVICE_VENDOR=0x1d6b
+MTP_DEVICE_PRODUCT=0x0100
+MTP_DEVICE_HCD_DRIVER=/sys/bus/pci/drivers/xhci_hcd
+MTP_DEVICE_HCD=0000:3c:00.0
+MANUFACTURER=Collabora
+PRODUCT="MTP Gadget"
+SERIAL="000000001"
+INTERFACE="Test MTP"
+INTERFACE_CLASS=Imaging
+INTERFACE_SUBCLASS="Still Image Capture"
+INTERFACE_PROTOCOL="Picture Transfer Protocol (PIMA 15470)"
