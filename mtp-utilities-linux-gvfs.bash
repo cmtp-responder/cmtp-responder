620cfe6e854ff019cc0243fa16071e333448946c
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-04-02 12:25:08 +0200

    Add linux gvfs backend
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

---
+++ mtp-utilities-linux-gvfs.bash
@@ -0,0 +1,93 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# (c) Collabora Ltd. 2019
+# Author: Andrzej Pietrasiewicz <andrzej.p@collabora.com>
+#
+
+mtp_prepare_tests_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2}"
+	local BASE=${__local_cookie[2]}
+	rm -rf "$BASE"/"$STORE/*"
+}
+
+mtp_list_files_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2}"
+	local PARENT="${3}"
+	local BASE=${__local_cookie[2]}
+	/usr/bin/find "$BASE"/"$STORE"/"$PARENT" -type f -printf "%P\n"
+	return 0
+}
+
+mtp_list_folders_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2}"
+	local PARENT="${3}"
+	local BASE=${__local_cookie[2]}
+	/usr/bin/find "$BASE"/"$STORE"/"$PARENT" -type d -printf "%P\n"
+	return 0
+}
+
+mtp_copy_to_store_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2}"
+	local PARENT="${3}"
+	local NAME="${4}"
+	local BASE=${__local_cookie[2]}
+	[ -d "$BASE"/"$STORE"/"$PARENT" ] || { echo "$PARENT does not exist"; return 1; }
+	cp "$NAME" "$BASE"/"$STORE"/"$PARENT"
+	return $?
+}
+
+mtp_retrieve_from_store_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2}"
+	local PARENT="${3}"
+	local REMOTE_NAME="${4}"
+	local LOCAL_NAME="${5}"
+	local BASE=${__local_cookie[2]}
+	[ -d "$BASE"/"$STORE"/"$PARENT" ] || { echo "$PARENT does not exist"; return 1; }
+	[ -f "$BASE"/"$STORE"/"$PARENT"/"$REMOTE_NAME" ] || { echo "$REMOTE_NAME does not exist" return 1; }
+	[ -r "$BASE"/"$STORE"/"$PARENT"/"$REMOTE_NAME" ] || { echo "$REMOTE_NAME is not readable"; return 1; }
+	/bin/cp "$BASE"/"$STORE"/"$PARENT"/"$REMOTE_NAME" "$LOCAL_NAME"
+	return $?
+}
+
+mtp_remove_from_store_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2}"
+	local PARENT="${3}"
+	local NAME="${4}"
+	local BASE=${__local_cookie[2]}
+	/bin/rm -rf "$BASE"/"$STORE"/"$PARENT"/"$NAME"
+	return 0
+}
+
+mtp_create_folder_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2#s:}"
+	local PARENT="${3#p:}"
+	local NAME="${4#n:}"
+	local BASE=${__local_cookie[2]}
+	[ -d "$BASE"/"$STORE"/"$PARENT" ] || { echo "$PARENT does not exist"; return 1; }
+	mkdir "$BASE"/"$STORE"/"$PARENT"/"$NAME"
+}
+
+mtp_remove_folder_linux_gvfs()
+{
+	local -n __local_cookie=${1}
+	local STORE="${2#s:}"
+	local PARENT="${3#p:}"
+	local NAME="${4#n:}"
+	local BASE=${__local_cookie[2]}
+	/bin/rm -rf "$BASE"/"$STORE"/"$PARENT"/"$NAME"
+}
--- mtp-utilities.bash
+++ mtp-utilities.bash
@@ -6,57 +6,58 @@
 #
 
 . mtp-utilities-bash-dummy.bash
+. mtp-utilities-linux-gvfs.bash
 
-mtp_prepare_tests_vtbl=(mtp_prepare_tests_bash_dummy)
+mtp_prepare_tests_vtbl=(mtp_prepare_tests_bash_dummy mtp_prepare_tests_linux_gvfs)
 
 mtp_prepare_tests()
 {
 	${mtp_prepare_tests_vtbl[$HOST]} $@
 }
 
-mtp_list_files_vtbl=(mtp_list_files_bash_dummy)
+mtp_list_files_vtbl=(mtp_list_files_bash_dummy mtp_list_files_linux_gvfs)
 
 mtp_list_files()
 {
 	${mtp_list_files_vtbl[$HOST]} $@
 }
 
-mtp_list_folders_vtbl=(mtp_list_folders_bash_dummy)
+mtp_list_folders_vtbl=(mtp_list_folders_bash_dummy mtp_list_folders_linux_gvfs)
 
 mtp_list_folders()
 {
 	${mtp_list_folders_vtbl[$HOST]} $@
 }
 
-mtp_copy_to_store_vtbl=(mtp_copy_to_store_bash_dummy)
+mtp_copy_to_store_vtbl=(mtp_copy_to_store_bash_dummy mtp_copy_to_store_linux_gvfs)
 
 mtp_copy_to_store()
 {
 	${mtp_copy_to_store_vtbl[$HOST]} $@
 }
 
-mtp_retrieve_from_store_vtbl=(mtp_retrieve_from_store_bash_dummy)
+mtp_retrieve_from_store_vtbl=(mtp_retrieve_from_store_bash_dummy mtp_retrieve_from_store_linux_gvfs)
 
 mtp_retrieve_from_store()
 {
 	${mtp_retrieve_from_store_vtbl[$HOST]} $@
 }
 
-mtp_remove_from_store_vtbl=(mtp_remove_from_store_bash_dummy)
+mtp_remove_from_store_vtbl=(mtp_remove_from_store_bash_dummy mtp_remove_from_store_linux_gvfs)
 
 mtp_remove_from_store()
 {
 	${mtp_remove_from_store_vtbl[$HOST]} $@
 }
 
-mtp_create_folder_vtbl=(mtp_create_folder_bash_dummy)
+mtp_create_folder_vtbl=(mtp_create_folder_bash_dummy mtp_create_folder_linux_gvfs)
 
 mtp_create_folder()
 {
 	${mtp_create_folder_vtbl[0]} $@
 }
 
-mtp_remove_folder_vtbl=(mtp_remove_folder_bash_dummy)
+mtp_remove_folder_vtbl=(mtp_remove_folder_bash_dummy mtp_remove_folder_linux_gvfs)
 
 mtp_remove_folder()
 {
