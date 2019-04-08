0e36e39e291a94d47ea40485261584b765d13219
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-04-08 22:04:25 +0200

    Add PowerShell mtp utilities
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

---
+++ mtp-utilities-windows-power-shell.bash
@@ -0,0 +1,54 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# (c) Collabora Ltd. 2019
+# Author: Andrzej Pietrasiewicz <andrzej.p@collabora.com>
+#
+
+mtp_prepare_tests_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
+
+mtp_list_files_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
+
+mtp_list_folders_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
+
+mtp_copy_to_store_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
+
+mtp_retrieve_from_store_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
+
+mtp_remove_from_store_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
+
+mtp_create_folder_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
+
+mtp_remove_folder_windows_power_shell()
+{
+	local -n __local_cookie=${1}
+	return 0
+}
