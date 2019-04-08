351c0f84cc8ca66207afdffc6417c464d5649746
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-04-08 12:03:20 +0200

    PowerShell backend for enumeartion tests
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- usb-utilities-windows.bash
+++ usb-utilities-windows.bash
@@ -7,11 +7,17 @@
 
 device_present_windows()
 {
+	DEV_VID=`echo $MTP_DEVICE_VENDOR | sed 's/0x//g'`
+	DEV_PID=`echo $MTP_DEVICE_PRODUCT | sed 's/0x//g'`
+	RESULT=`powershell.exe "c:\\Users\\Administrator\\cmtp-responder-tests\\device-present.ps1" -VendorId $DEV_VID -ProductId $DEV_PID | head -c 1`
+	[ "$RESULT" == "1" ] || { echo "No device present"; return 1; }
 	return 0
 }
 
 device_is_mtp_device_windows()
 {
+	RESULT=`powershell.exe "c:\\Users\\Administrator\\cmtp-responder-tests\\device-is-mtp.ps1" -Manufacturer \'$MANUFACTURER\' -Product \'$PRODUCT_WINDOWS\' -Serial \'$SERIAL\' -DeviceClass \'$INTERFACE_CLASS_WINDOWS\' -DeviceSubclass \'$INTERFACE_SUBCLASS_WINDOWS\' -Protocol \'$INTERFACE_PROTOCOL_WINDOWS\' | head -c 1`
+	[ "$RESULT" == "1" ] || { echo "Device is not mtp device"; return 1; }
 	return 0
 }
 
