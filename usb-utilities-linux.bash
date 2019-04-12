83c323e6b985bf7bb28a3ddfb5df499e91516f52
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-04-12 17:36:35 +0200

    Add and unify error messages from usb utilities
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- usb-utilities-linux.bash
+++ usb-utilities-linux.bash
@@ -10,6 +10,7 @@ device_present_linux()
 	run lsusb -d $MTP_DEVICE_VENDOR:$MTP_DEVICE_PRODUCT
 	[ "$status" -eq 0 ] && return 0
 
+	echo "Cannot find MTP device!"
 	return 1
 }
 
@@ -23,28 +24,28 @@ lsusb_field_4()
 device_is_mtp_device_linux()
 {
 	local LSUSB_MANUFACTURER=`lsusb_field_4 iManufacturer`
-	[ "$LSUSB_MANUFACTURER" == "$MANUFACTURER" ] || return 1
+	[ "$LSUSB_MANUFACTURER" == "$MANUFACTURER" ] || { echo "Unexpected manufacturer: $LSUSB_MANUFACTURER"; return 1; }
 
 	local LSUSB_PRODUCT=`lsusb_field_4 iProduct`
-	[ "$LSUSB_PRODUCT" == "$PRODUCT" ] || return 1
+	[ "$LSUSB_PRODUCT" == "$PRODUCT" ] || { echo "Unexpected product: $LSUSB_PRODUCT"; return 1; }
 
 	local LSUSB_SERIAL=`lsusb_field_4 iSerial`
-	[ "$LSUSB_SERIAL" == "$SERIAL" ] || return 1
+	[ "$LSUSB_SERIAL" == "$SERIAL" ] || { echo "Unexpected serial number: $LSUSB_SERIAL"; return 1; }
 
 	local LSUSB_NUM_INTERFACES=`lsusb -d $MTP_DEVICE_VENDOR:$MTP_DEVICE_PRODUCT -v | grep iInterface | wc -l`
-	[ $LSUSB_NUM_INTERFACES -eq 1 ] || return 1
+	[ $LSUSB_NUM_INTERFACES -eq 1 ] || { echo "Unexpected number of interfaces: $LSUSB_NUM_INTERFACES"; return 1; }
 
 	local LSUSB_INTERFACE=`lsusb_field_4 iInterface`
-	[ "$LSUSB_INTERFACE" == "$INTERFACE" ] || return 1
+	[ "$LSUSB_INTERFACE" == "$INTERFACE" ] || { echo "Unexpected interface: $LSUSB_INTERFACE"; return 1; }
 
 	local LSUSB_INTERFACE_CLASS=`lsusb_field_4 bInterfaceClass`
-	[ "$LSUSB_INTERFACE_CLASS" == "$INTERFACE_CLASS" ] || return 1
+	[ "$LSUSB_INTERFACE_CLASS" == "$INTERFACE_CLASS" ] || { echo "Unexpected interface class: $LSUSB_INTERFACE_CLASS"; return 1; }
 
 	local LSUSB_INTERFACE_SUBCLASS=`lsusb_field_4 bInterfaceSubClass`
-	[ "$LSUSB_INTERFACE_SUBCLASS" == "$INTERFACE_SUBCLASS" ] || return 1
+	[ "$LSUSB_INTERFACE_SUBCLASS" == "$INTERFACE_SUBCLASS" ] || { echo "Unexpected interface subclass: $LSUSB_INTERFACE_SUBCLASS"; return 1; }
 
 	local LSUSB_INTERFACE_PROTOCOL=`lsusb_field_4 bInterfaceProtocol`
-	[ "$LSUSB_INTERFACE_PROTOCOL" == "$INTERFACE_PROTOCOL" ] || return 1
+	[ "$LSUSB_INTERFACE_PROTOCOL" == "$INTERFACE_PROTOCOL" ] || { echo "Unexpected protocol: $LSUSB_INTERFACE_PROTOCOL"; return 1; }
 
 	lsusb -d $MTP_DEVICE_VENDOR:$MTP_DEVICE_PRODUCT -v | grep -A2 bEndpointAddress | awk \
 		'BEGIN{bulk_in=0;bulk_out=0;int_in=0;int_out=0;current_type=-1} \
