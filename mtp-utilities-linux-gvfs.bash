c94870052dbea17ebeb91ac51703793d753f5080
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-04-12 17:42:15 +0200

    Report error upon unsuccessful gvfs mount
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- mtp-utilities-linux-gvfs.bash
+++ mtp-utilities-linux-gvfs.bash
@@ -10,10 +10,12 @@ mtp_prepare_tests_linux_gvfs()
 	local -n __local_cookie=${1}
 	local STORE="${2}"
 	local BASE=${__local_cookie[2]}
+
 	run gio mount -u ${__local_cookie[3]} 2>&1 > /dev/null
 	sleep 1
 	run gio mount ${__local_cookie[3]} 2>&1 > /dev/null
-	sleep 1
+	[ $? -eq 0 ] || { echo "Cannot mount ${__local_cookie[3]}"; return 1; }
+
 	find "$BASE"/"$STORE"/ -type f -print0 | xargs -0 gio remove -f
 	find "$BASE"/"$STORE"/ -type d -print0 | sort -r -z | xargs -0 gio remove -f
 	return 0
@@ -25,7 +27,9 @@ mtp_list_files_linux_gvfs()
 	local STORE="${2}"
 	local PARENT="${3}"
 	local BASE=${__local_cookie[2]}
+
 	/usr/bin/find "$BASE"/"$STORE"/"$PARENT" -type f -printf "%P\n"
+
 	return 0
 }
 
@@ -35,7 +39,9 @@ mtp_list_folders_linux_gvfs()
 	local STORE="${2}"
 	local PARENT="${3}"
 	local BASE=${__local_cookie[2]}
+
 	/usr/bin/find "$BASE"/"$STORE"/"$PARENT" -type d -printf "%P\n"
+
 	return 0
 }
 
@@ -46,8 +52,10 @@ mtp_copy_to_store_linux_gvfs()
 	local PARENT="${3}"
 	local NAME="${4}"
 	local BASE=${__local_cookie[2]}
+
 	[ -d "$BASE"/"$STORE"/"$PARENT" ] || { echo "$PARENT does not exist"; return 1; }
 	gio copy "$NAME" "$BASE"/"$STORE"/"$PARENT"
+
 	return $?
 }
 
@@ -59,10 +67,12 @@ mtp_retrieve_from_store_linux_gvfs()
 	local REMOTE_NAME="${4}"
 	local LOCAL_NAME="${5}"
 	local BASE=${__local_cookie[2]}
+
 	[ -d "$BASE"/"$STORE"/"$PARENT" ] || { echo "$PARENT does not exist"; return 1; }
 	[ -f "$BASE"/"$STORE"/"$PARENT"/"$REMOTE_NAME" ] || { echo "$REMOTE_NAME does not exist" return 1; }
 	[ -r "$BASE"/"$STORE"/"$PARENT"/"$REMOTE_NAME" ] || { echo "$REMOTE_NAME is not readable"; return 1; }
 	gio copy "$BASE"/"$STORE"/"$PARENT"/"$REMOTE_NAME" "$LOCAL_NAME"
+
 	return $?
 }
 
@@ -73,7 +83,9 @@ mtp_remove_from_store_linux_gvfs()
 	local PARENT="${3}"
 	local NAME="${4}"
 	local BASE=${__local_cookie[2]}
+
 	gio remove -f "$BASE"/"$STORE"/"$PARENT"/"$NAME"
+
 	return $?
 }
 
@@ -84,8 +96,10 @@ mtp_create_folder_linux_gvfs()
 	local PARENT="${3#p:}"
 	local NAME="${4#n:}"
 	local BASE=${__local_cookie[2]}
+
 	[ -d "$BASE"/"$STORE"/"$PARENT" ] || { echo "$PARENT does not exist"; return 1; }
 	gio mkdir "$BASE"/"$STORE"/"$PARENT"/"$NAME"
+
 	return $?
 }
 
@@ -96,8 +110,10 @@ mtp_remove_folder_linux_gvfs()
 	local PARENT="${3#p:}"
 	local NAME="${4#n:}"
 	local BASE=${__local_cookie[2]}
+
 	find "$BASE"/"$STORE"/"$PARENT"/"$NAME" -type f -print0 | xargs -0 gio remove -f
 	find "$BASE"/"$STORE"/"$PARENT"/"$NAME" -type d -print0 | sort -r -z | xargs -0 gio remove -f
+
 	return 0
 }
 
