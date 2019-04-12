edfbf69a270599b06c73a3114d272c3d106f0055
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-04-12 11:22:19 +0200

    Factor out bash dummy os setup
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- os-setup.bash
+++ os-setup.bash
@@ -5,23 +5,14 @@
 # Author: Andrzej Pietrasiewicz <andrzej.p@collabora.com>
 #
 
+. os-setup-bash-dummy.bash
 . os-setup-linux-gvfs.bash
-
-os_setup_bash_dummy()
-{
-	true
-}
 
 os_setup_vtbl=(os_setup_bash_dummy os_setup_linux_gvfs)
 
 os_setup()
 {
 	${os_setup_vtbl[$HOST]} $@
-}
-
-os_teardown_bash_dummy()
-{
-	true
 }
 
 os_teardown_vtbl=(os_teardown_bash_dummy os_teardown_linux_gvfs)
