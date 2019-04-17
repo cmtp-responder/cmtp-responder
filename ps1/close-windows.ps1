754ff0ca1789d58fe23af7072617712f4a8702b4
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2019-04-17 11:45:37 +0200

    Fix closing folder windows in PowerShell backend
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

--- ps1/close-windows.ps1
+++ ps1/close-windows.ps1
@@ -7,8 +7,10 @@
 
 param($store)
 
+$windows_to_close = @('child', 'folder6', 'subdir', 'folder5', 'subfolder', 'folder4', 'folder3', 'folder2', 'folder', $store)
+
 $windows=(New-Object -comObject Shell.Application).Windows()
 
-$windows | where-object {$_.LocationName -eq 'subfolder'} | foreach-object {$_.quit()}
-$windows | where-object {$_.LocationName -eq 'folder'} | foreach-object {$_.quit()}
-$windows | where-object {$_.LocationName -eq $store} | foreach-object {$_.quit()}
+foreach ($title in $windows_to_close) {
+	$windows | where-object {$_.LocationName -eq $title} | foreach-object {$_.quit()}
+}
