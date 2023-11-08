fd83e4e7081cde6c224f73b43fffed9b95276786
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2023-11-08 14:57:20 +0100

    build: Add Dockerfile for generating a reproducible build environment
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>

---
+++ Docker/armhf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WORKDIR="${WORKDIR:-${HOME}}"
+
+case "${1}" in
+	run)
+		docker run -it \
+			--network=host \
+			-v ${WORKDIR}:/home/build \
+			-w /home/build \
+			-u $(id -u):$(id -g) \
+			--hostname cmtp-responder-build \
+			cmtp-responder-build:armhf
+		;;
+	build)
+		docker buildx --target cmtp-responder-build-armhf \
+			-t cmtp-responder-build:armhf \
+			.
+		;;
+	*)
+		echo "Usage: $0 {run|build}"
+		exit 1
+esac
